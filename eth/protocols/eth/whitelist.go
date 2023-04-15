9179bd469a47350850903105f9ae9d1cbd392d31
Ciaran Hughes <iamciaran2@gmail.com>
2023-04-15 01:10:50 +0100

    Updated whitelist.go with entire functionality to read in whitelist

--- eth/protocols/eth/whitelist.go
+++ eth/protocols/eth/whitelist.go
@@ -1,17 +1,67 @@
 package eth
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
 )
 
-// Read in the whitelist_addresses.json file
-// Todo: Make this a command flag
+// Hardcode the whitelist_addresses.json file path
+// Todo: Make this a command line flag parameter
 
+// The whitelist file should be a json array of addresses
 const (
 	whitelist_filename string = "whitelist_addresses.json"
 )
 
+// Whitelist contains relevant information about the Whitelist parameters
 type Whitelist struct {
-	WhitelistFilePathFlag string
-	WhiteListedAddresses  []common.Address
+	WhitelistFilePath    string
+	WhitelistedAddresses map[common.Address]bool // a map for optimized complexity of operations
+}
+
+// Make a New Whitelist and initialize it
+func NewWhitelist() *Whitelist {
+
+	wl := &Whitelist{
+		WhitelistFilePath: whitelist_filename,
+	}
+
+	wl.ReadWhitelistFile()
+	log.Info("Read in whitelist file ", wl.WhitelistFilePath,
+		". Only allowing these addresses in transaction/hash propagation")
+
+	return wl
+}
+
+// Set the file path for the whitelist object
+func (wl *Whitelist) SetFilePath() {
+	wl.WhitelistFilePath = whitelist_filename
+}
+
+// Set the file path for the whitelist object
+func (wl *Whitelist) ReadWhitelistFile() {
+
+	// Read in the whitelist file
+	bytes, err := os.ReadFile(wl.WhitelistFilePath)
+	if err != nil {
+		log.Warn("There was an Error ", err, " Opening the Whitelist File Path : ",
+			wl.WhitelistFilePath, ". Defaulting to not using a whitelist")
+	}
+
+	var wl_array []common.Address
+
+	// Unmarshal the bytes
+	if err := json.Unmarshal(bytes, &wl_array); err != nil {
+		log.Warn("There was an Error ", err, " Unmarshaling the Whitelist File from Path : ",
+			wl.WhitelistFilePath, ". Note the format of the file is expected to be ",
+			" a json array of addresses as strings. Defaulting to not using a whitelist")
+	}
+
+	for _, address := range wl_array {
+		wl.WhitelistedAddresses[address] = true
+	}
+
 }
